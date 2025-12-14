1870f92f6276544a71328b38829429403cd608f2
asdfxwev <167267637+asdfxwev@users.noreply.github.com>
2025-12-14 14:37:12 +0900

    롤백

--- src/main/java/com/example/groupware/board/BoardController.java
+++ src/main/java/com/example/groupware/board/BoardController.java
@@ -21,7 +21,7 @@ public class BoardController {
     }
 
     // 글 수정
-    @PutMapping("/boardList/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<BoardResponseDto> updateBoard(
             @PathVariable Long id,
             @RequestBody BoardRequestDto request) {
@@ -29,7 +29,7 @@ public class BoardController {
     }
 
     // 글 삭제하기
-    @DeleteMapping("/board/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBoard(@PathVariable Long id) {
         boardService.deleteBoard(id);
         return ResponseEntity.noContent().build();
